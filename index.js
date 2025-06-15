f1fff2be8541509b081e62655d962ea939b39ad7
halil gezer <hg_1993@windowslive.com>
2025-06-16 02:13:48 +0300

    Port numarası 4000'den 3000'e güncellendi

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const firmaRoutes = require('./routes/firmaRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
